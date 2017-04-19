14eb3bfe7908acbc48fe4246717791c0aba80114
Chui Tey <chui.tey@readify.net>
2017-04-19 23:11:58 +1000

    Pasting images

---
+++ MarkdownDeep.Img.js
@@ -0,0 +1,86 @@
+﻿"use strict";
+
+/**
+ * Markdown image uploader
+ */
+
+function MarkdownImageUploader(el, options) {
+
+    var self = this;
+
+    function closeModal() {
+        this.addEventListener('click',
+            function (event) {
+                $(event.target).parents(".mdd_modal").fadeOut("fast").remove();
+            });
+    }
+
+    function showModal(dataUrl, okPrompt) {
+        var container = $(this._el).parents('.mdd_editor_wrap').parent();
+        var modalHtml =
+            '<div class="mdd_modal">               \n' +
+            ' <div class="mdd_modal_frame">        \n' +
+            '   <div class="mdd_modal_button">     \n' +
+            '      <a href="#">Close</a>           \n' +
+            '   </div>                             \n' +
+            '   <div class="mdd_modal_content">    \n' +
+            '      <img src="" class="mdd_image" />\n' +
+            '   </div>                             \n' +
+            '   <button class="btn btn-primary">   \n' +
+            '      OK                              \n' +
+            '   </button>                          \n' +
+            ' </div>                               \n' +
+            '</div>                                \n';
+        var modalElement = $(modalHtml);
+        container.prepend(modalElement);
+        modalElement.fadeIn("fast");
+        modalElement.find(".mdd_modal_button").each(closeModal);
+        modalElement.find("img").attr("src", dataUrl);
+
+        return new Promise(function (resolve, reject) {
+            modalElement.find("button.btn-primary")
+                .html(okPrompt)
+                .on("click", function () {
+                    resolve();
+                    modalElement.remove();
+                });
+        });
+    }
+
+    function onPaste(event) {
+        var items = (event.clipboardData || event.originalEvent.clipboardData).items;
+        for (var index in items) {
+            var item = items[index];
+            if (item.kind === "file" || item.type && item.type.indexOf("image") !== -1) {
+                var blob = item.getAsFile();
+                var reader = new FileReader();
+                reader.onload = function (event) {
+                    var dataUrl = event.target.result;
+                    self.showModal(dataUrl, "Upload image").then(function () {
+                        if (options.onPaste) {
+                            options.onPaste(self._mdd, self._el, blob);
+                        }
+                    });
+                };
+                reader.readAsDataURL(blob);
+            }
+        }
+    }
+
+   function activate() {
+        this._el = el;
+        this._mdd = $(el).data("mdd");
+        this.onPaste = onPaste;
+        this.showModal = showModal;
+        this._el.onPaste = this.onPaste.bind(this);
+    }
+
+    activate.call(this);
+}
+
+$.fn.MarkdownDeepEnablePasteImage = function (options) {
+    this.each(function () {
+        var uploader = new MarkdownImageUploader(this, options || {});
+    });
+    return this;
+}
---
+++ README.md
@@ -0,0 +1,31 @@
+#MarkdownDeep demo with pasting image from the clipboard
+
+## Live demo
+
+Note the live demo does not upload image to a server,
+as you are looking at a static site.
+
+You will have to run the program on your own machine
+to see it work.
+
+## Installation
+
+Execute the following in the command line
+
+    npm install
+    npm start
+
+and then browse to http://localhost:3000/
+
+## Supported browsers
+
+Chrome, Microsoft Edge
+
+## Does not work
+
+This demo does not work on Microsoft Internet Explorer 11
+because the "paste" event does not fire when the clipboard
+contains images, and the textarea is focussed.
+
+
+
